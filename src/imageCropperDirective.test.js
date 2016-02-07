a2dae47e9d44e83de7924daf082fa56e90cdf1e0
bcabanes <contact@benjamincabanes.com>
2016-02-07 14:15:15 -0500

    test(test): Add more tests and get more coverage

--- src/imageCropperDirective.test.js
+++ src/imageCropperDirective.test.js
@@ -37,90 +37,90 @@ module.exports = function(angular) {
       expect(element.html()).to.contain('imgCropper-image');
     });
 
-    //it('should do a basic crop with specs: w400 | h300 | f0 | r1 | z1', function(done) {
-    //  var testReference = require('./imageTestReferences/w400-h300-z1-f1-r0'),
-    //    imageUrl = 'data:image/jpeg;base64,' + testReference.original,
-    //    $scope = $rootScope.$new();
-    //
-    //  $scope.getCropperApi = function(api) {
-    //    var image = api.crop();
-    //    var test = image.replace(image.substr(0, image.indexOf(',') + 1), '');
-    //    expect(test).to.equal(testReference.cropped);
-    //    done();
-    //  };
-    //
-    //  var template = '<image-cropper image-url="' + imageUrl + '" api="getCropperApi" show-controls="false"></image-cropper>';
-    //  var element = $compile(template)($scope);
-    //  $scope.$digest();
-    //});
+    it('should do a basic crop with specs: w400 | h300 | f0 | r1 | z1', function(done) {
+      var testReference = require('./imageTestReferences/w400-h300-z1-f1-r0'),
+        imageUrl = 'data:image/jpeg;base64,' + testReference.original,
+        $scope = $rootScope.$new();
 
-    //it('should do a basic crop with specs: w400 | h300 | f1 | r270 | z3', function(done) {
-    //  var testReference = require('./imageTestReferences/w400-h300-z3-f1-r270'),
-    //    imageUrl = 'data:image/jpeg;base64,' + testReference.original,
-    //    $scope = $rootScope.$new();
-    //
-    //  $scope.getCropperApi = function(api) {
-    //    api.zoom(3);
-    //    api.rotate(270);
-    //    api.fit();
-    //    var image = api.crop();
-    //    var test = image.replace(image.substr(0, image.indexOf(',') + 1), '');
-    //    expect(test).to.equal(testReference.cropped);
-    //    done();
-    //  };
-    //
-    //  var template = '<image-cropper image-url="' + imageUrl + '" api="getCropperApi" show-controls="false"></image-cropper>';
-    //  var element = $compile(template)($scope);
-    //  $scope.$digest();
-    //});
+      $scope.getCropperApi = function(api) {
+        var image = api.crop();
+        var test = image.replace(image.substr(0, image.indexOf(',') + 1), '');
+        expect(test).to.equal(testReference.cropped);
+        done();
+      };
 
-    //it('should throw an error if degrees aren\'t %90', function(done) {
-    //  var testReference = require('./imageTestReferences/w400-h300-z3-f1-r270'),
-    //    imageUrl = 'data:image/jpeg;base64,' + testReference.original,
-    //    $scope = $rootScope.$new();
-    //
-    //  $scope.getCropperApi = function(api) {
-    //    expect(function() {
-    //      api.zoom(3);
-    //      api.rotate(25);
-    //    }).to.throw(Error, /Support only multiple of 90° for rotation./gi);
-    //    done();
-    //  };
-    //
-    //  var template = '<image-cropper image-url="' + imageUrl + '" api="getCropperApi"></image-cropper>';
-    //  var element = $compile(template)($scope);
-    //  $scope.$digest();
-    //});
+      var template = '<image-cropper image-url="' + imageUrl + '" api="getCropperApi" show-controls="false"></image-cropper>';
+      var element = $compile(template)($scope);
+      $scope.$digest();
+    });
 
-    //it('should give an error of tainted canvases with external image src', function(done) {
-    //  var imageUrl = 'http://dummyimage.com/100x100',
-    //    $scope = $rootScope.$new();
-    //
-    //  $scope.getCropperApi = function(api) {
-    //    expect(api.crop).to.throw(Error, /tainted canvases/gi);
-    //    done();
-    //  };
-    //
-    //  var template = '<image-cropper image-url="' + imageUrl + '" api="getCropperApi"></image-cropper>';
-    //  var element = $compile(template)($scope);
-    //  $scope.$digest();
-    //});
+    it('should do a basic crop with specs: w400 | h300 | f1 | r270 | z3', function(done) {
+      var testReference = require('./imageTestReferences/w400-h300-z3-f1-r270'),
+        imageUrl = 'data:image/jpeg;base64,' + testReference.original,
+        $scope = $rootScope.$new();
 
-    //it('should remove itself properly', function(done) {
-    //  var testReference = require('./imageTestReferences/w400-h300-z3-f1-r270'),
-    //    imageUrl = 'data:image/jpeg;base64,' + testReference.original,
-    //    $scope = $rootScope.$new();
-    //
-    //  $scope.getCropperApi = function(api) {
-    //    api.remove();
-    //    $scope.$digest();
-    //    expect(element.html()).to.be.empty;
-    //    done();
-    //  };
-    //
-    //  var template = '<image-cropper image-url="' + imageUrl + '" api="getCropperApi"></image-cropper>';
-    //  var element = $compile(template)($scope);
-    //  $scope.$digest();
-    //});
+      $scope.getCropperApi = function(api) {
+        api.zoom(3);
+        api.rotate(270);
+        api.fit();
+        var image = api.crop();
+        var test = image.replace(image.substr(0, image.indexOf(',') + 1), '');
+        expect(test).to.equal(testReference.cropped);
+        done();
+      };
+
+      var template = '<image-cropper image-url="' + imageUrl + '" api="getCropperApi" show-controls="false"></image-cropper>';
+      var element = $compile(template)($scope);
+      $scope.$digest();
+    });
+
+    it('should throw an error if degrees aren\'t %90', function(done) {
+      var testReference = require('./imageTestReferences/w400-h300-z3-f1-r270'),
+        imageUrl = 'data:image/jpeg;base64,' + testReference.original,
+        $scope = $rootScope.$new();
+
+      $scope.getCropperApi = function(api) {
+        expect(function() {
+          api.zoom(3);
+          api.rotate(25);
+        }).to.throw(Error, /Support only multiple of 90° for rotation./gi);
+        done();
+      };
+
+      var template = '<image-cropper image-url="' + imageUrl + '" api="getCropperApi"></image-cropper>';
+      var element = $compile(template)($scope);
+      $scope.$digest();
+    });
+
+    it('should give an error of tainted canvases with external image src', function(done) {
+      var imageUrl = 'http://dummyimage.com/100x100',
+        $scope = $rootScope.$new();
+
+      $scope.getCropperApi = function(api) {
+        expect(api.crop).to.throw(Error);
+        done();
+      };
+
+      var template = '<image-cropper image-url="' + imageUrl + '" api="getCropperApi"></image-cropper>';
+      var element = $compile(template)($scope);
+      $scope.$digest();
+    });
+
+    it('should remove itself properly', function(done) {
+      var testReference = require('./imageTestReferences/w400-h300-z3-f1-r270'),
+        imageUrl = 'data:image/jpeg;base64,' + testReference.original,
+        $scope = $rootScope.$new();
+
+      $scope.getCropperApi = function(api) {
+        api.remove();
+        $scope.$digest();
+        expect(element.html()).to.be.empty;
+        done();
+      };
+
+      var template = '<image-cropper image-url="' + imageUrl + '" api="getCropperApi"></image-cropper>';
+      var element = $compile(template)($scope);
+      $scope.$digest();
+    });
   });
 };
